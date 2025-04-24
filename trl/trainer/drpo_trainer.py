e48b1e9fc66798aba2badbcd78c426a15961328f
ErhanXu <schmidtxhanschen@gmail.com>
2025-04-25 07:52:08 +0800

    add print generations

--- trl/trainer/drpo_trainer.py
+++ trl/trainer/drpo_trainer.py
@@ -696,7 +696,7 @@ class DRPOTrainer(Trainer):
                         preference_score_star[~contain_eos_token] -= self.args.missing_eos_penalty
                     
                     generated_examples = self.processing_class.batch_decode(prompt_astar_ids[0,:], skip_special_tokens=True)
-                    print("generated_examples: ", generated_examples[0])
+                    print("generated_examples: ", generated_examples)
 
                     del (prompt_astar_ids, prompt_a2_repeated_ids, prompt_astar_attention_mask, prompt_a2_repeated_attention_mask)
                 if not args.loss2_only:
